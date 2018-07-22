4b26e7b6c6c5b7b60308d0ecc38521f5b76a2189
unknown <vip.arman2011@gmail.com>
2018-07-22 23:58:22 +0400

    stateless to sttatefull

--- src/libs/ModalZ.jsx
+++ src/libs/ModalZ.jsx
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import { cx, css } from 'emotion';
 import '../assets/index.scss';
 
-const body = css`
-  align-self: flex-start;
-  width: 100%;
-  margin: 0.5rem;
-  background: #fff;
-  padding: 1rem;
-  position: relative;
-`;
 const setSize = (size) => {
   switch (size) {
     case 'large':
@@ -23,58 +15,48 @@ const setSize = (size) => {
   }
 };
 
-const ModalZ = ({
-  size, text, children, isOpen,
-}) => {
-  const isOpened = isOpen;
-  const holder = css`
-    @import url('https://fonts.googleapis.com/css?family=Roboto');
-    font-family: 'Roboto', sans-serif;
+class ModalZ extends Component {
+  constructor(props) {
+    super(props);
+    this.closeModal = this.closeModal.bind(this);
+  }
 
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    padding: 1em;
-    background-color: rgba(0, 0, 0, 0.85);
-    line-height: 1;
-    display: flex;
-    justify-content: center;
-    user-select: none;
-    z-index: 1000;
-    transition: background-color 0.5s linear;
-  `;
-  const opened = css`
-    opacity: 1;
-    visibility: 'visible';
-  `;
-  const closed = css`
-    opacity: 0;
-    visibility: hidden;
-  `;
+  closeModal = () => (e) => {
+    const modal = document.getElementById('modalZHolder');
+    modal.classList.remove('opened');
+    modal.classList.add('closed');
+  };
 
-  return (
-    <div id="modalZHolder" className={holder}>
+  render() {
+    const {
+      size, text, children, isOpen,
+    } = this.props;
+
+    return (
       <div
-        id="modalZ"
-        className={body}
-        role="presentation"
-        css={`
-          @media (min-width: 992px) {
-            width: ${setSize(size)};
-          }
-          @media (min-width: 576px) {
-            width: ${setSize(size)};
-            margin: 1.75rem auto;
-          }
-        `}
+        onClick={this.closeModal()}
+        id="modalZHolder"
+        className={`holder ${isOpen ? 'opened' : 'closed'}`}
       >
-        {children || text}
+        <div
+          id="modalZ"
+          className="body"
+          role="presentation"
+          css={`
+            @media (min-width: 992px) {
+              width: ${setSize(size)};
+            }
+            @media (min-width: 576px) {
+              width: ${setSize(size)};
+              margin: 1.75rem auto;
+            }
+          `}
+        >
+          {children || text}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ModalZ;
